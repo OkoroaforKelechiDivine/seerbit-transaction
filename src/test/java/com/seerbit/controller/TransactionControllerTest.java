9576db9623ba149e8be218a56b3c6f9c3efb2001
OkoroaforKelechiDivine <106816493+okoroaforkelechidivine@users.noreply.github.com>
2022-10-26 18:38:05 +0100

    finished working on the controller layer

---
+++ src/test/java/com/seerbit/controller/TransactionControllerTest.java
@@ -0,0 +1,10 @@
+package com.seerbit.controller;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class TransactionControllerTest {
+
+}
