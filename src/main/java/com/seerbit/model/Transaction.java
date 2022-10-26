2f8024cd72fdc447f4345ee5708c501074fc2ae0
OkoroaforKelechiDivine <106816493+okoroaforkelechidivine@users.noreply.github.com>
2022-10-26 17:56:52 +0100

    finished working on the model layer

--- src/main/java/com/seerbit/model/Transaction.java
+++ src/main/java/com/seerbit/model/Transaction.java
@@ -1,4 +1,21 @@
 package com.seerbit.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Transaction {
+
+    @NonNull
+    private String amount;
+
+    private LocalDateTime timestamp;
+
 }
