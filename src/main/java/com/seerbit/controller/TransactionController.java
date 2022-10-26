00723ad95ccab17b613f90d1d5b7021685b32ff2
OkoroaforKelechiDivine <106816493+okoroaforkelechidivine@users.noreply.github.com>
2022-10-26 18:53:20 +0100

    finished working on the controller layer

--- src/main/java/com/seerbit/controller/TransactionController.java
+++ src/main/java/com/seerbit/controller/TransactionController.java
@@ -6,20 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/transaction")
+@RequestMapping("/transactions")
 public class TransactionController {
 
     @Autowired
     private TransactionService transactionService;
-
 
     @PostMapping
     public Transaction createTransaction(@RequestBody Transaction transaction){
         return transactionService.saveTransaction(transaction);
     }
 
-
-    @GetMapping({"id"})
+    @GetMapping("/statistics/{id}")
     public Transaction findTransactionById(@PathVariable int id){
         return transactionService.getTransactionById(id);
     }
